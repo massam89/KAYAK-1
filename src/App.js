7270fa97b1ab015791944e0ab3f365e8e220918b
Masoud <mas.sam1368@gmail.com>
2022-03-08 16:05:43 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import jsonp from 'jsonp';
 
 function App() {
+
+  useEffect(()=>{
+   
+    jsonp('https://www.kayak.com/h/mobileapis/directory/airlines/homework', {param: 'jsonp'}, (err, data) => {
+      if(err){
+
+      }else {
+        console.log(data)
+      }
+    })
+    
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
